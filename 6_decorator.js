9e4a922adbee3e3cf235507dc7a22487360ba143
asia-tango <kaplunoleksandra@gmail.com>
2020-05-11 00:08:07 +0300

    Added new server

--- 6_decorator.js
+++ 6_decorator.js
@@ -17,6 +17,16 @@ function aws(server) {
   return server
 }
 
+function azure(server) {
+  server.isAzure = true;
+  server.port += 500
+  return server;
+}
+
 const s1 = aws(new Server('12.36.56.78', 8080));
-console.log(sq.isAWS);
+console.log(s1.isAWS);
 console.log(s1.aswInfo());
+
+const s2 = azure(new Server('98.76.54.32', 1000));
+console.log(s2.isAzure);
+console.log(s2.url);
